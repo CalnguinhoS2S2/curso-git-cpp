e13c57804e95298260a718546f3c66c9551acc50
Erick Lucas <erick.lucas0973@gmail.com>
2026-09-25 13:59:42 -0300

    feat: multi

--- main.cpp
+++ main.cpp
@@ -5,5 +5,5 @@ int main() {
     cout << "Olá, Git!\n";
     int a = 10, b = 20;
     cout<<"a= "<<a<<" b= "<<b<<endl;
-    cout<<"subtracao a-b = "<<a-b<<endl;
+    cout<<"multi a*b = "<<a*b<<endl;
 }
